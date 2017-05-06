07ded1750512bbcf42918fa5654732035995520d
arunraj2017 <arunraj.nexus5@gmail.com>
2017-05-05 21:27:23 -0400

    frst

---
+++ AngularPromise/WebContent/appPromise.js
@@ -0,0 +1,46 @@
+/**
+ * promise definition
+ */
+
+"use strict"
+
+		angular
+			.module("appPromise", [])
+			.factory("getData", function($timeout, $q) {
+				return function() {
+					var defer = $q.defer();
+					$timeout(function() {
+						defer.resolve("data resolved success");
+					},2000)
+					return defer.promise;
+				}
+				
+			})
+			.factory("getData2", function($q, $timeout) {
+				return $q(function(resolve, reject) {
+					$timeout(function() {
+						if(Math.round(Math.random())){
+							resolve("data received success");
+						}else{
+							reject("data failure");
+						}
+					},2000);
+					
+				});
+				
+			})
+			.run(function(getData, getData2, $log) {
+				var promise = getData().then(function(data) {
+					console.log(data);
+				});
+				
+				var promise2 = getData2.then(function(value) {
+					$log.info(value);
+				}, function(reason) {
+					$log.info(reason);
+				}, function(value) {
+					$log.info(value);
+				})
+			});
+			
+		
